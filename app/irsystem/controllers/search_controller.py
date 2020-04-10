c4852c5a7f3fd3db716f7a6e7cf7e23fd80dd0b3
stephanie <sc2524@cornell.edu>
2020-04-10 18:39:52 -0400

    reddit data

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,7 +2,7 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "The Perfect Party"
+project_name = "The Perfect Neighborhood"
 net_id = "Stephanie Chang (sc2524), Kati Hsu (kyh24), Robert Zhang (rdz26), Sneha Kumar (sk2279), Shirley Kabir (szk4)"
 
 @irsystem.route('/', methods=['GET'])
