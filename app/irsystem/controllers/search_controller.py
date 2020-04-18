51ea0c45b7c1b40b0f6247189241d106a59e2753
SK1723 <sk2279@cornell.edu>
2020-04-18 15:48:07 -0400

    Updated template and controller to handle basic input

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,20 +1,29 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 project_name = "The Perfect Neighborhood"
 net_id = "Stephanie Chang (sc2524), Kati Hsu (kyh24), Robert Zhang (rdz26), Sneha Kumar (sk2279), Shirley Kabir (szk4)"
 
-@irsystem.route('/', methods=['GET'])
+@irsystem.route('/', methods=['POST','GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
+	if request.method == 'POST':
+		age = request.form["age"]
+		commute_type = request.form["commute-type"]
+		safety = request.form["safety"]
+		budget = request.form["budget"]
+		some = request.form["some"]
+	else:
+		age = request.args.get('age')
+		commute_type = request.args.get('commute-type')
+		safety = request.args.get('safety')
+		budget = request.args.get('budget')
+		some = request.args.get('some')
+
+	if not age:
 		data = []
 		output_message = ''
 	else:
-		output_message = "Your search: " + query
+		output_message = "Your age: " + age + " Commute: " + commute_type + " Safety: " + safety + " Budget: " + budget + " Some: " + some
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
