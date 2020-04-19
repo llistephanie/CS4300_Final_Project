695f923d095c308bdacfb49e4e90164a9f6b269c
shirleykabir <shirleykabir@gmail.com>
2020-04-19 17:20:41 -0400

    keyword dictionary

--- app/irsystem/controllers/calculate.py
+++ app/irsystem/controllers/calculate.py
@@ -39,6 +39,25 @@ neighborhood_list = ['Battery Park',
                      'Washington Heights',
                      'West Village']
 
+relevant_keywords = {"Coffee Shops": ["coffee shops", "tea", "coffee", "cafe", "cafes", "coffee shop", "coffee stores", "bakeries"],
+                     "Working Out": ["working out", "gym", "yoga", "run", "skating"],
+                     "Watching Movies": ["watching movies", "movie theatre", "movies", "movie"],
+                     "Nightlife": ["nightlife", "bars", "going out", "clubs", "rooftops", "party", "cocktail", "drinking", "partiers"],
+                     "Music": ["music", "entertainment", "jazz", "performance", "performances", "concert", "talent"],
+                     "Theater": ["theatre", "entertainment", "house theaters", "broadway", "performances", "off-Broadway", "dance", "drama", "talent"],
+                     "Restaurants": ["restaurants", "restaurant", "foodie", "foodies", "food", "eateries", "culinary", "cuisine", "bistros", "dining"],
+                     "Shopping": ["shopping", "shopper", "boutiques", "shopper's", "commercial", "fashion-forward", "fashion", "retailers", "commerce"],
+                     "Art": ["art", "artsy", "architecture", "buildings", "artists", "gallery", "galleries", "artistic", "photographers", "sculptors", "painters", "trendy", "bohemian", "creative", "museum", "museums", "picturesque", "creative"],
+                     "Outdoors": ["outdoors", "parks", "park", "recreation", "waterfront", "public spaces", "outdoor spaces", "trees", "flowers", "garden", "gardens", "picnics", "green", "nature", "greenspace", "green spaces", "open spaces"],
+                     "Expensive": ["expensive", "pricey", "luxury", "affluent", "posh", "doorman", "elevator"],
+                     "Affordable": ["affordable", "inexpensive", "below-market", "diverse budgets"],
+                     "Quiet": ["quiet", "escape", "peaceful", "serene", "calm", "laid-back", "tranquil", "mellow", "low-key", "early to bed", "secluded", "simplicity", "empty", "uncluttered", "simple", "slower", "relaxed"],
+                     "Loud": ["loud", "lively", "fast-paced", "congested", "energetic", "traffic", "hustle", "noise", "vibrant", "packed"], 
+                     "Old": ["old"], 
+                     "Young": ["young", "students", "younger"], 
+                     "Modern": ["modern", "high-rises", "skyscrapers", "lofts"], 
+                     "Rustic": ["rustic", "pre-war", "historic", "brownstones", "historical", "walk-ups", "old-world", "character"]}
+
 """
 Shared data containing all the scores and information for each neighborhood.
 Scores will be a value from 
@@ -51,71 +70,78 @@ Includes the following components:
 """
 data = {}
 for x in neighborhood_list:
-	data[x] = {}
+    data[x] = {}
 
 
 def mergeDict(original, updates, key_name):
-    for k,v in updates.items():
-        new_val={key_name: v}
+    for k, v in updates.items():
+        new_val = {key_name: v}
         original[k].update(new_val)
 
+
 def loadCrimeScores():
-	"""
-	Function adds in the attributes "description" and "safety_score" for each neighborhood.
-	Description is pulled from the niche data
-	Safety score ranges from (0-100)
-	"""
-	# global data # declare global in order to update global data variable
-    
-	with open("app/irsystem/controllers/data/safety.json","r") as f:
-		input_data = json.load(f)
-	mergeDict(data, input_data, "safety score")
-	return input_data
-	# data.update(input_data)
+    """
+    Function adds in the attributes "description" and "safety_score" for each neighborhood.
+    Description is pulled from the niche data
+    Safety score ranges from (0-100)
+    """
+    # global data # declare global in order to update global data variable
+
+    with open("app/irsystem/controllers/data/safety.json", "r") as f:
+        safety_data = json.load(f)
+
+    percentages = np.array([float(v) for k, v in safety_data.items()])
+    normalized = (percentages-min(percentages)) / \
+        (max(percentages)-min(percentages))*100
+    norm_safety_scores = {
+        neighborhood_list[i]: v for i, v in enumerate(normalized)}
+    mergeDict(data, norm_safety_scores, "safety score")
+    return norm_safety_scores
+
 
 def calculateAgeScore(age):
     """
     Input:
         age         value (int) if user included it, None otherwise
-    
+
     Output:
         age_scores  dictionary indexed by neighborhood of score (0-100) assigned to each
     """
-
-    print(f"[AGE] {data}")
-    if age=='':
-        age=24
+    if age == '':
+        age = 24
     else:
-        age=int(age)
+        age = int(age)
 
     with open("app/irsystem/controllers/data/niche.json") as f:
         niche_data = json.load(f)
-    
-    age_dist=["<10 years", "10-17 years", "18-24 years", "25-34 years", "35-44 years", "45-54 years", "55-64 years","65+ years"]
 
-    age_dist=""
-    if age<10:
-        age_dist="<10 years"
-    elif age<=17:
-        age_dist="10-17 years"
-    elif age<=24:
-        age_dist="18-24 years"
-    elif age<=34:
-        age_dist="25-34 years"
-    elif age<=44:
-        age_dist="35-44 years"
-    elif age<=54:
-        age_dist="45-54 years"
-    elif age<=64:
-        age_dist="55-64 years"
+    age_dist = ["<10 years", "10-17 years", "18-24 years", "25-34 years",
+                "35-44 years", "45-54 years", "55-64 years", "65+ years"]
+
+    age_dist = ""
+    if age < 10:
+        age_dist = "<10 years"
+    elif age <= 17:
+        age_dist = "10-17 years"
+    elif age <= 24:
+        age_dist = "18-24 years"
+    elif age <= 34:
+        age_dist = "25-34 years"
+    elif age <= 44:
+        age_dist = "35-44 years"
+    elif age <= 54:
+        age_dist = "45-54 years"
+    elif age <= 64:
+        age_dist = "55-64 years"
     else:
-        age_dist="65+ years"
+        age_dist = "65+ years"
 
     # for k,v in niche_data.items():
     if not age:
         return None
 
-    percentages=np.array([int(v["age distribution"][age_dist].replace('%', '')) for k,v in niche_data.items()])
+    percentages = np.array([int(v["age distribution"][age_dist].replace(
+        '%', '')) for k, v in niche_data.items()])
     # percentages=preprocessing.normalize(percentages.reshape(1, -1)).flatten()
     # [0.02673567 0.13367837 0.16041405 0.34756377 0.14704621 0.25398891
     # 0.18714972 0.13367837 0.30746026 0.24062107 0.13367837 0.12031053
@@ -123,7 +149,7 @@ def calculateAgeScore(age):
     # 0.45450646 0.09357486 0.08020702 0.22725323 0.09357486 0.08020702
     # 0.17378188 0.12031053 0.04010351 0.08020702 0.06683919 0.06683919
     # 0.13367837 0.08020702]
-    
+
     # percentages=percentages-np.mean(percentages)/np.std(percentages)
     # [ 0.3814625  8.3814625 10.3814625 24.3814625  9.3814625 17.3814625
     # 12.3814625  8.3814625 21.3814625 16.3814625  8.3814625  7.3814625
@@ -132,96 +158,107 @@ def calculateAgeScore(age):
     # 11.3814625  7.3814625  1.3814625  4.3814625  3.3814625  3.3814625
     # 8.3814625  4.3814625 ]
 
-    normalized=(percentages-min(percentages))/(max(percentages)-min(percentages))*100
+    normalized = (percentages-min(percentages)) / \
+        (max(percentages)-min(percentages))*100
     # # [0.      0.25    0.3125  0.75    0.28125 0.53125 0.375   0.25    0.65625
     # # 0.5     0.25    0.21875 0.25    0.5625  0.1875  0.1875  0.25    0.125
     # # 1.      0.15625 0.125   0.46875 0.15625 0.125   0.34375 0.21875 0.03125
     # # 0.125   0.09375 0.09375 0.25    0.125  ]
 
-    norm_age_scores={ neighborhood_list[i] :v for i,v in enumerate(normalized)}
+    norm_age_scores = {neighborhood_list[i]                       : v for i, v in enumerate(normalized)}
 
     # data.update(norm_age_scores)
     mergeDict(data, norm_age_scores, "age score")
     return norm_age_scores
 
+
 def calculateBudget(minBudget, maxBudget):
     with open("app/irsystem/controllers/data/renthop.json") as f:
         renthop_data = json.load(f)
 
-    fit_budget=[]
-    print(f"[BUDGET] {data}")
+    fit_budget = []
     # top_25s=[]
-    # essentially finding percentage of homes under [min,max] range 
-    for k,v in renthop_data.items():
+    # essentially finding percentage of homes under [min,max] range
+    for k, v in renthop_data.items():
 
-        bottom=int(v.get("Studio", v.get("1BR"))["Bottom 25%"].replace('$','').replace(',', ''))
-        median=int(v.get("Studio", v.get("1BR"))["Median"].replace('$','').replace(',', ''))
-        top=int(v.get("Studio", v.get("1BR"))["Top 25%"].replace('$','').replace(',', ''))
+        bottom = int(v.get("Studio", v.get("1BR"))[
+                     "Bottom 25%"].replace('$', '').replace(',', ''))
+        median = int(v.get("Studio", v.get("1BR"))[
+                     "Median"].replace('$', '').replace(',', ''))
+        top = int(v.get("Studio", v.get("1BR"))[
+                  "Top 25%"].replace('$', '').replace(',', ''))
         # top_25s.append(top)
 
-        my_range=set(list(range(minBudget, maxBudget)))
-        n_range=set(list(range(bottom, top)))
+        my_range = set(list(range(minBudget, maxBudget)))
+        n_range = set(list(range(bottom, top)))
 
-        intersect=my_range.intersection(n_range)
-        percentage_points=50.0/(top-bottom)
-        
-        if(len(intersect)==0):
+        intersect = my_range.intersection(n_range)
+        percentage_points = 50.0/(top-bottom)
+
+        if(len(intersect) == 0):
             fit_budget.append(0)
         else:
-            fit_budget.append((max(intersect)-min(intersect))*percentage_points)
+            fit_budget.append((max(intersect)-min(intersect))
+                              * percentage_points)
 
-    fit_budget=np.array(fit_budget)
-    normalized=(fit_budget-min(fit_budget))/(max(fit_budget)-min(fit_budget))*100
+    fit_budget = np.array(fit_budget)
+    normalized = (fit_budget-min(fit_budget)) / \
+        (max(fit_budget)-min(fit_budget))*100
 
     # keywords={}
     # if maxBudget>=mean(top_25s):
-        # for text analysis
-    norm_budget_scores={ neighborhood_list[i] :v for i,v in enumerate(normalized)}
+    # for text analysis
+    norm_budget_scores = {
+        neighborhood_list[i]: v for i, v in enumerate(normalized)}
 
     # data.update(norm_budget_scores)
     mergeDict(data, norm_budget_scores, "budget score")
     return norm_budget_scores
 
+
 def calculateCommuteScore(commuteType):
     with open("app/irsystem/controllers/data/walkscore.json") as f:
         walkscore_data = json.load(f)
-    
-    print(f"[COMMUTE] {data}")
-    
-    type_key={'walk': "walk score", 'bike': "bike score", 'public transit': "transit score", 'car': "transit score"}
+    type_key = {'walk': "walk score", 'bike': "bike score",
+                'public transit': "transit score", 'car': "transit score"}
 
-    commute_scores=np.array([int(v['rankings'][type_key[commuteType.lower()]]) for k,v in walkscore_data.items()])
-    normalized=(commute_scores-min(commute_scores))/(max(commute_scores)-min(commute_scores))*100
-    norm_commute_scores={ neighborhood_list[i] :v for i,v in enumerate(normalized)}
+    commute_scores = np.array(
+        [int(v['rankings'][type_key[commuteType.lower()]]) for k, v in walkscore_data.items()])
+    normalized = (commute_scores-min(commute_scores)) / \
+        (max(commute_scores)-min(commute_scores))*100
+    norm_commute_scores = {
+        neighborhood_list[i]: v for i, v in enumerate(normalized)}
     mergeDict(data, norm_commute_scores, "commute score")
     return norm_commute_scores
 
+
 def getTopNeighborhoods(query):
 
-    with open("app/irsystem/controllers/data/neighborhoods.json","r") as f:
+    with open("app/irsystem/controllers/data/neighborhoods.json", "r") as f:
         all_data = json.load(f)
-    
+
     with open("app/irsystem/controllers/data/niche.json") as f:
         niche_data = json.load(f)
-    
+
     loadCrimeScores()
     calculateBudget(int(query['budget-min']), int(query['budget-max']))
     calculateAgeScore(query['age'])
     calculateCommuteScore(query['commute-type'])
-    safetyWeight=0.25*(int(query['safety'])/5)
-    otherWeights=(1.0-safetyWeight)/3
+    safetyWeight = 0.25*(int(query['safety'])/5)
+    otherWeights = (1.0-safetyWeight)/3
 
+    neighborhood_scores = []
+    for k, v in data.items():
+        score = otherWeights*v['budget score']+otherWeights*v['age score'] + \
+            otherWeights*v['commute score']+safetyWeight*v['safety score']
+        neighborhood_scores.append((k, score))
+    top_neighborhoods = sorted(
+        neighborhood_scores, key=lambda x: x[1], reverse=True)[:10]
 
-    neighborhood_scores=[]
-    for k,v in data.items():
-        print(v['safety score'])
-        score=otherWeights*v['budget score']+otherWeights*v['age score']+otherWeights*v['commute score']+safetyWeight*v['safety score']
-        neighborhood_scores.append((k,score))
-    top_neighborhoods=sorted(neighborhood_scores, key = lambda x: x[1], reverse=True)[:10]
-
-    best_matches=[]
-    for (name,score) in top_neighborhoods:
-        n={'name': name, 'score': round(score, 2), 'image-url': all_data[name]['images'].split(',')[0], 'description': niche_data[name]['description']}
+    best_matches = []
+    for (name, score) in top_neighborhoods:
+        n = {'name': name, 'score': round(score, 2), 'image-url': all_data[name]['images'].split(
+            ',')[0], 'description': niche_data[name]['description']}
         best_matches.append(n)
     return best_matches
 
--- app/static/script.js
+++ app/static/script.js
@@ -1,11 +1,51 @@
-$(function () {
-  // updateBudget();
+/**
+ * http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+ *
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+function hslToRgb(h, s, l) {
+  var r, g, b;
 
-  $(".chosen-select").chosen({
-    no_results_text: "Oops, nothing found!",
-  });
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    }
 
-});
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+
+  return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+}
+
+// convert a number to a color using hsl
+function numberToColorHsl(i) {
+  // as the function expects a value between 0 and 1, and red = 0° and green = 120°
+  // we convert the input to the appropriate hue value
+  var hue = (i * 1.2) / 360;
+  // we convert hsl to rgb (saturation 100%, lightness 50%)
+  var rgb = hslToRgb(hue, 1, 0.5);
+  // we format to css value and return
+  return "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
+}
 
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -29,25 +69,39 @@ function updateBudgetLabels() {
 
 function updateSafetyLabel() {
   score = parseInt(parseInt($("#safety").val()));
-  label = ""
+  label = "";
 
   switch (score) {
     case 1:
-      label="not at all important"
+      label = "not at all important";
       break;
     case 2:
-      label="not very important"
+      label = "not very important";
       break;
     case 3:
-      label="somewhat important"
+      label = "somewhat important";
       break;
     case 4:
-      label="very important"
+      label = "very important";
       break;
     default:
-      label="extremely important"
+      label = "extremely important";
   }
   $(".safety").text(label);
-
-
 }
+
+$(function () {
+  // updateBudget();
+
+  $(".chosen-select").chosen({
+    no_results_text: "Oops, nothing found!",
+  });
+  
+  $( ".result h5" ).each(function( index ) {
+    score = parseFloat($( this ).text());
+    $(this).css("color", numberToColorHsl(score));
+    console.log( index + ": " + $( this ).text() );
+  });
+
+  
+});
