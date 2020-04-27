e21736c581aaae38a761e84e838822514806cee4
shirleykabir <shirleykabir@gmail.com>
2020-04-27 02:46:38 -0400

    warnings

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from app import app, socketio
+import sys
+
+cli = sys.modules['flask.cli']
+cli.show_server_banner = lambda *x: None
 
 if __name__ == "__main__":
   print("Flask app running at http://0.0.0.0:5000")
