ceedaae9715f75a9fd60ce48ac656f818bc535e1
shirleykabir <shirleykabir@gmail.com>
2020-05-04 05:48:46 -0400

    datetime

--- app/irsystem/controllers/calculate.py
+++ app/irsystem/controllers/calculate.py
@@ -9,8 +9,9 @@ import nltk
 from gensim.models import Word2Vec
 from nltk.corpus import wordnet
 import googlemaps
-from datetime import datetime
 from nltk import tokenize
+import datetime
+from time import mktime
 # Full list of neighborhoods
 # NOTE: if you use these as keys, you can simply update the shared data dictionary variable (data)
 
@@ -952,10 +953,16 @@ def calculateCommuteScore(commuteType, commuteDestination, commuteDuration, comm
         all_matrices={}
 
         all_durations={}
+
+        today = datetime.date.today()
+        monday=today + datetime.timedelta(days=(7 - today.weekday()))
+        nineam = datetime.time(9, 0)
+        monday_9am=datetime.datetime.combine(monday, nineam)
+
+        timestamp_monday_9am=mktime(monday_9am.timetuple())
         
         for k,v in travel_modes.items():
-            # nineam = datetime.datetime.combine(datetime.date.today(), datetime.time(9, 0))
-            all_matrices[k] = gmaps.distance_matrix(place_ids, commuteDestination, mode=v)
+            all_matrices[k] = gmaps.distance_matrix(place_ids, commuteDestination, mode=v, departure_time=timestamp_monday_9am)
             # print(json.dumps(all_matrices[k], indent=4))
             # print(k)
             all_durations[k] = {neighborhood_list[i]: int(v['elements'][0]['duration']['value']/60) if 'duration' in v['elements'][0].keys() else None for i, v in enumerate(all_matrices[k]['rows']) }
