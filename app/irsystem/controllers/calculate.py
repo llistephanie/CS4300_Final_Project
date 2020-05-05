4b451c3974628fddf3eae366d92e9879307d076c
shirleykabir <shirleykabir@gmail.com>
2020-05-04 23:22:17 -0400

    decimals

--- app/irsystem/controllers/calculate.py
+++ app/irsystem/controllers/calculate.py
@@ -939,7 +939,6 @@ def calculateCommuteScore(commuteType, commuteDestination, commuteDuration, comm
             # print(k)
             all_durations[k] = {neighborhood_list[i]: int(v['elements'][0]['duration']['value']/60) if 'duration' in v['elements'][0].keys() else None for i, v in enumerate(all_matrices[k]['rows']) }
 
-        # print(json.dumps(all_matrices['Public Transit'], indent=4))
 
         ratio=int(float(commuteDuration)+1)/(np.array([v['elements'][0]['duration']['value']/60 if 'duration' in v['elements'][0].keys() else 160.0 for v in all_matrices[commuteType]['rows']])+1)
 
